c6968e88fe81126619dbe729591363ce856b7619
neeraj9436 <60649504+neeraj9436@users.noreply.github.com>
2022-02-08 07:50:06 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    // apiKey: "AIzaSyCDL3U26Kvu16VxUcpsRtpL07dEIDwIgpQ",
+    // authDomain: "fir-project-a7458.firebaseapp.com",
+    // projectId: "fir-project-a7458",
+    // storageBucket: "fir-project-a7458.appspot.com",
+    // messagingSenderId: "575906290952",
+    // appId: "1:575906290952:web:0f2f7421abffc0db1420bd",
+    // measurementId: "G-6EF9ND3KRX"
+    apiKey: "AIzaSyDeE-63sV4rl84fKuTjp3mKiTzI4jRHwXE",
+    authDomain: "login-25307.firebaseapp.com",
+    projectId: "login-25307",
+    storageBucket: "login-25307.appspot.com",
+    messagingSenderId: "54639101509",
+    appId: "1:54639101509:web:49507752cd0d46a59e852c",
+    measurementId: "G-BFX5SPWXZX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
